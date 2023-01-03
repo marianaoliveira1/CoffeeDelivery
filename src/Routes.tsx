21d638fe1ad04f0f35835cc790f5d25fffcd347f
Mariana Oliveira <marianaoliveirafernandes@alu.ufc.br>
2023-01-03 10:31:43 -0300

    adicionado as imagens

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { DefaultLayout } from "./layouts/DefaultLayout"
 import { CompleteOrderPage } from "./pages/CompleteOrder"
 import { HomePage } from "./pages/Home"
 import { OrderConfirmedPage } from "./pages/OrderConfirmed"
@@ -8,9 +9,12 @@ export function Router() {
         <div>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={ <HomePage /> } />
-                    <Route path="/completeOrder" element={ <CompleteOrderPage /> } />
-                    <Route path="/completeConfirmed" element={ <OrderConfirmedPage /> } />
+                    <Route path="/" element={ <DefaultLayout />}>
+                       <Route path="/" element={ <HomePage /> } />
+                        <Route path="/completeOrder" element={ <CompleteOrderPage /> } />
+                        <Route path="/completeConfirmed" element={ <OrderConfirmedPage /> } /> 
+                    </Route>
+                    
                 </Routes>
             </BrowserRouter>
         </div>
